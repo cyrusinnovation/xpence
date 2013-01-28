4d5b5ee5ef19d3cb0b0524a69b3fd7cebe9b424a
Matthew Salerno <matthewsalern@gmail.com>
2013-01-28 13:51:13 -0500

    tests for new fields in employee create form

--- test/integration/admin_flows_test.rb
+++ test/integration/admin_flows_test.rb
@@ -7,11 +7,19 @@ class AdminFlowsTest < ActionDispatch::IntegrationTest
   end
     
   
-  test "admin has field to create new employee with email address" do 
+  test "admin can create new employee with all relevant attributes" do 
     login_with_oauth
     visit('/employees/new')
+    fill_in('Name', :with => 'Robert Smith')
+    fill_in('Cardnumber', :with => 'XXXX-XX-1446')
     fill_in('Email', :with => 'rsmith@cyrusinnovation.com')
-    assert page.has_content?('asdkjkjh'), "does not have content"
+    check('Admin')
+    click_button('Create Employee')
+    robert = Employee.find_by_name("Robert Smith")
+    assert robert
+    assert_equal robert.cardnumber, 'XXXX-XX-1446'
+    assert_equal robert.email, 'rsmith@cyrusinnovation.com'
+    assert robert.admin?
   end
 
 end
